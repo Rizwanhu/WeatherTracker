13780269521fb0102dcd0e316ec83f28db4667c0
Rizwanhu <rizwanhussain66@hotmail.com>
2025-01-27 10:48:54 -0800

    Added heading

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from '../public/styles/Home.module.scss';
 import WeatherCard from './WeatherCard';
-
+// new here line
 const Home = () => {
   return (
     <main className={styles.container}>
